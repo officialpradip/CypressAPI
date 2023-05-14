73b373cdc61b91ed6cf9264c6bece7a716ecb8e0
officialpradip <study.pradipthapa@gmail.com>
2023-05-14 16:08:21 +0545

    get

--- cypress/e2e/jsonparse.cy.js
+++ cypress/e2e/jsonparse.cy.js
@@ -1,1 +1,10 @@
 /// <reference types="Cypress"/>
+
+describe("JSON Parse",()=>{
+    cy.request({
+        method:"GET",
+        url:'https://fakestoreapi.com/products'
+    }).then((result)=>{
+        expect(result.status).to.equal(200)
+    })
+})
