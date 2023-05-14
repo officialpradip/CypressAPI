d14b49f3d5144282af3c100d8765dc55308ededb
officialpradip <study.pradipthapa@gmail.com>
2023-05-14 17:23:14 +0545

    loop

--- cypress/e2e/jsonparse.cy.js
+++ cypress/e2e/jsonparse.cy.js
@@ -2,13 +2,28 @@
 
 describe("JSON Parse",()=>{
     it("demo",()=>{
-
-    
     cy.request({
         method:'GET',
         url:'https://fakestoreapi.com/products'
     }).then((res)=>{
         expect(res.status).to.eq(200)
+    })
+})
+let total=0;
+it("demo loop",()=>{
+    let total=0
+    cy.request({
+        method:'GET',
+        url:'https://fakestoreapi.com/products',
+        qs:{limit:5}
+    }).then((res)=>{
+        expect(res.status).to.eq(200)
+        res.body.forEach(element => {
+            total=total+ element.price
+            
+        });
+        expect(total).to.eq(899.23)
+        
     })
 })
 })
